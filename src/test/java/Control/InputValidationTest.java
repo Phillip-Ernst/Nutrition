734f8894735c2612096d5d14636ba681aeee7b2c
Phillip Ernst <ernstphillip102@gmail.com>
2024-04-28 12:23:44 -0500

    commit

---
+++ src/test/java/Control/InputValidationTest.java
@@ -0,0 +1,55 @@
+package Control;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InputValidationTest {
+
+    private InputValidation validator;
+
+    @Before
+    public void setUp() {
+        validator = new InputValidation();
+    }
+
+    @Test
+    public void testUsernamePasswordValidation_ValidInput() {
+        assertTrue(validator.UsernamePasswordValidation("JohnDoe123", "SecurePass123"));
+    }
+
+    @Test
+    public void testUsernamePasswordValidation_InvalidUsername() {
+        assertFalse(validator.UsernamePasswordValidation("JohnDoe123;", "SecurePass123"));
+    }
+
+    @Test
+    public void testUsernamePasswordValidation_InvalidPassword() {
+        assertFalse(validator.UsernamePasswordValidation("JohnDoe123", "Secure;Pass"));
+    }
+
+    @Test
+    public void testUsernamePasswordValidation_TooLongUsername() {
+        assertFalse(validator.UsernamePasswordValidation("JohnDoe12345678901234567890", "SecurePass123"));
+    }
+
+    @Test
+    public void testUsernamePasswordValidation_TooLongPassword() {
+        assertFalse(validator.UsernamePasswordValidation("JohnDoe123", "SecurePass12345678901234567890"));
+    }
+
+    @Test
+    public void testFoodInputValidation_ValidInput() {
+        assertTrue(validator.foodInputValidation("Apple"));
+    }
+
+    @Test
+    public void testFoodInputValidation_InvalidInput() {
+        assertFalse(validator.foodInputValidation("Choco;late"));
+    }
+
+    @Test
+    public void testFoodInputValidation_TooLongInput() {
+        assertFalse(validator.foodInputValidation("SuperCalifragilisticexpialidociousFood"));
+    }
+}
